628ba92151bb48c5a0aa06153a6fb9517a6dadc2
Christian Surlykke <christian@surlykke.dk>
2025-07-20 21:59:28 +0200

    Set shiftwith and friends to 4

--- init.lua
+++ init.lua
@@ -39,6 +39,12 @@ vim.o.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 10
 
+vim.o.ts = 4
+vim.o.shiftwidth = 4
+vim.o.smartindent = true
+
+vim.o.title = true
+
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
